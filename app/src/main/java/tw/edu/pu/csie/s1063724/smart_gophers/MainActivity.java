978cfab595ac1201c7dae9a665894211d279e80a
user <boymiffy@gmail.com>
2020-06-10 20:50:43 +0800

    新增音樂功能

--- app/src/main/java/tw/edu/pu/csie/s1063724/smart_gophers/MainActivity.java
+++ app/src/main/java/tw/edu/pu/csie/s1063724/smart_gophers/MainActivity.java
@@ -2,13 +2,45 @@ package tw.edu.pu.csie.s1063724.smart_gophers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity<mPlayer> extends AppCompatActivity {
+    MediaPlayer mPlayer;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        try{
+            mPlayer = MediaPlayer.create(this, R.raw.music);
+            mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mPlayer.setLooping(true);
+        }catch (IllegalStateException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        mPlayer.start();
+    }
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        mPlayer.pause();
+    }
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        mPlayer.release();
     }
 }
