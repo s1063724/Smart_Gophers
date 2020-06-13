125d2a8347f9c2bd0efdfd29dd1b822298ee9439
Wuray <s1063724@gm.pu.edu.tw>
2020-06-13 23:56:40 +0800

    製作歡迎畫面&遊戲導入畫面

--- app/src/main/java/tw/edu/pu/csie/s1063724/smart_gophers/MainActivity.java
+++ app/src/main/java/tw/edu/pu/csie/s1063724/smart_gophers/MainActivity.java
@@ -3,12 +3,16 @@ package tw.edu.pu.csie.s1063724.smart_gophers;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Set fullscreen
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
